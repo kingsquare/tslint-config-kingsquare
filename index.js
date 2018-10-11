105ed9e1c785e2b6192cd16f4a471f9562b5a3ec
Tim de Koning <git@king-management.nl>
2018-10-11 21:44:23 +0200

    v 1.0.1 --- update to typescript 3

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 module.exports = {
     extends: 'tslint-config-airbnb',
     rules: {
-        "max-line-length": [true, 120],
+        "max-line-length": [
+            true,
+            {
+                "limit": 120,
+                "ignore-pattern": "\\s*<" //jsx hack https://twitter.com/timtyrrell/status/689912501165658112
+            }
+        ],
         "react/jsx-filename-extension": [0],
         "react/prefer-stateless-function": [0],
         "react/react-in-jsx-scope": [0],
@@ -10,9 +16,5 @@ module.exports = {
         "no-param-reassign": [0], // e.g. reduce array to object
         "no-underscore-dangle": [0],
         "default-case": [0], // no default case in a switch is fine...
-        "max-len": ["error", 120, {
-            "ignoreTemplateLiterals": true,
-            "ignorePattern": "\\s*<" //jsx hack https://twitter.com/timtyrrell/status/689912501165658112
-        }]
     }
 };
