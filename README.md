b07494979fbc6aab069f3a45b3378615af8e0318
Tim de Koning <git@king-management.nl>
2018-10-26 10:58:27 +0200

    Release v5.0.0
    Bump version --- now in line with default tslint config

--- README.md
+++ README.md
@@ -16,3 +16,17 @@ also create a tslint.json:
 ```
 
 That should do it!
+
+## NOTE
+
+Semver major number is based on tslint version.
+
+## Use of Prettier
+
+This config should work nicely with prettier. A config-file .prettierrc is adviced with
+
+```
+{
+  "singleQuote": true
+}
+```
--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 module.exports = {
-    extends: 'tslint-config-airbnb',
-    rules: {
-        "max-line-length": [true, 120],
-        "react/jsx-filename-extension": [0],
-        "react/prefer-stateless-function": [0],
-        "react/react-in-jsx-scope": [0],
-        "global-require": [1],
-        "comma-dangle": [0],
-        "no-param-reassign": [0], // e.g. reduce array to object
-        "import-name": [0], // import React from 'react' is fine...
-        "default-case": [0], // no default case in a switch is fine...
-        "max-len": ["error", 120, {
-            "ignoreTemplateLiterals": true,
-            "ignorePattern": "\\s*<" //jsx hack https://twitter.com/timtyrrell/status/689912501165658112
-        }]
-    }
+  "extends": [
+    "tslint:latest",
+    "tslint-config-prettier"
+  ],
+  "rules": {
+    "object-literal-sort-keys": false, // cannot be done automatically. Takes up too much time.
+    "no-namespace": false // Is official typescript feature. Don't understand why this should not be allowed
+  }
 };
